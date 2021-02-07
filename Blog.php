af6828101fa5d6c15743f9fca486c1c5fbd99c64
Mukhammadamin Abdullaev <mukhammadaminabdullaev01082002>
2021-02-08 01:12:41 +0500

    Pagination functionalities add on Blog file

--- Blog.php
+++ Blog.php
@@ -88,10 +88,19 @@
 					$stmt = $ConnectingDB->prepare($sql);
 					$stmt->bindValue(':search', '%'.$Search.'%');
 					$stmt->execute();
+				} // Query when pagination is Active i.e Blog.php?Page=1
+				elseif (isset($_GET["page"])) {
+					$Page = $_GET["page"];
+					if ($Page == 0 || $Page < 1) {
+						$ShowPostFrom = 0;
+					} else { $ShowPostFrom = ($Page * 5) - 5;
+						}
+					$sql = "SELECT * FROM posts ORDER BY id desc LIMIT $ShowPostFrom,5";
+					$stmt = $ConnectingDB->query($sql);
 				}
 				// The default SQL query
 				else{
-					$sql = "SELECT * FROM posts ORDER BY id desc";
+					$sql = "SELECT * FROM posts ORDER BY id desc LIMIT 0, 3";
 					$stmt = $ConnectingDB->query($sql);
 				}
 				while ($DataRows = $stmt->fetch()) {
@@ -109,8 +118,10 @@
 				 	<img src="Uploads/<?php echo htmlentities($Image); ?>" style="max-height: 450px" class="img-fluid card-img-top" alt="Failed To Upload Image" />
 				 	<div class="card-body">
 				 		<h4 class="card-title"><?php echo htmlentities($PostTitle); ?></h4>
-				 			<small class="text-muted">Written by: <?php echo $Admin ?> On <?php echo htmlentities($DateTime) ?></small>
-				 			<span style="float: right;" class="badge badge-dark text-light">Comments 20</span>
+				 			<small class="text-muted">Category: <span class="text-dark"><?= htmlentities($Category); ?></span> | Written by: <span class="text-dark"><?php echo $Admin ?></span> On <span class="text-dark"><?php echo htmlentities($DateTime) ?></span></small>
+				 			<span style="float: right;" class="badge badge-dark text-light">Comments
+				 				<?= ApproveCommentsAccordingToPost($PostId); ?>
+				 			</span>
 				 			<hr>
 				 			<p class="card-text">
 				 				<?php 
@@ -123,10 +134,55 @@ echo htmlentities($PostDescription);
 				 			<a href="FullPost.php?id=<?php echo $PostId; ?>" style="float: right;"><span class="btn btn-info">Read More >></span></a>
 				 	</div>
 				 </div>
+				 <br>
 				<?php } ?>
+				<!-- Pagination -->
+				<nav>
+					<ul class="pagination pagination-lg">
+						<!-- Creating Backward Button -->
+                      <?php if (isset($Page)) {
+                      	if ($Page > 1) { ?>
+	                  	<li class="page-item">
+								<a href="Blog.php?page=<?php echo $Page - 1; ?>" class="page-link">&laquo;</a>
+						</li>
+					<?php } }?>
+						<?php
+ 
+                        global $ConnectingDB;
+                        $sql = "SELECT COUNT(*) FROM posts";
+                        $stmt = $ConnectingDB->query($sql);
+                        $RowPagination = $stmt->fetch();
+                        $TotalPosts = array_shift($RowPagination);
+                        // echo $TotalPosts . "<br>";
+                        $PostPagination = $TotalPosts / 5;
+                        $PostPagination = ceil($PostPagination);
+                        // echo $PostPagination;
+                        for ($i=1; $i <= $PostPagination ; $i++) {
+                        	if (isset($Page)) {
+                        		if ($i == $Page) { ?>
+                        			
+						<li class="page-item active">
+							<a href="Blog.php?page=<?php echo $i; ?>" class="page-link"><?php echo $i; ?></a>
+						</li>
+						<?php } else { ?>
+
+						<li class="page-item">
+							<a href="Blog.php?page=<?php echo $i; ?>" class="page-link"><?php echo $i; ?></a>
+						</li>
+
+                      <?php } } }?>
+
+                      <!-- Creating Forward Button -->
+                      <?php if (isset($Page) && !empty($Page)) {
+                      	if ($Page + 1 <= $PostPagination) { ?>
+	                  	<li class="page-item">
+								<a href="Blog.php?page=<?php echo $Page + 1; ?>" class="page-link">&raquo;</a>
+						</li>
+					<?php } }?>
+					</ul>
+				</nav>
 			</div>
 			<!-- Main Area End -->
-
 
 
             <!-- Side Area Start -->
